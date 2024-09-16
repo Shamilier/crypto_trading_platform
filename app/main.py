1235357dd6f086dc8b8016d1f3d08bfb176c9ed9
Shamilier <Shamilgaliev18@mail.ru>
2024-09-16 17:06:39 +0300

    CORS

--- app/main.py
+++ app/main.py
@@ -3,12 +3,21 @@ from .routes import auth_routes
 from .database import init, close
 from starlette.middleware.sessions import SessionMiddleware
 from starlette.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 @app.on_event("startup")
 async def startup():
     await init()
+    
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Разрешить запросы с любых доменов
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
