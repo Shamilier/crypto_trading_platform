a1e55b59219103036bea74037d28f59491818d92
Shamil <shamilgaliev18mail.ru@MacBook-Pro-Shamil.local>
2025-04-22 11:50:19 +0300

    server test_2

--- app/main.py
+++ app/main.py
@@ -24,7 +24,7 @@ async def shutdown():
 app.add_middleware(
     CORSMiddleware,
     # allow_origins=["http://localhost:3000"],  # Указываем точный адрес фронтенда
-    allow_origins=["http://localhost:8000"],
+    allow_origins=["*"],
     allow_credentials=True,  # Разрешаем куки
     allow_methods=["*"],  # Разрешаем все методы (GET, POST и т.д.)
     allow_headers=["*"],  # Разрешаем все заголовки
