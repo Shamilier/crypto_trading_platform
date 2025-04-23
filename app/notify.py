fe997ae96e270cae2c416f8c36dda7c4f4e69faa
Shamil <shamilgaliev18mail.ru@MacBook-Pro-Shamil.local>
2025-04-23 15:09:03 +0300

    notify_final_final

--- app/notify.py
+++ app/notify.py
@@ -37,11 +37,11 @@ def main():
     was_running = True
     while True:
         if not is_container_running() and was_running:
-            msg = f"🚨🚨🚨 Контейнер {CONTAINER_NAME} остановлен! 🚨🚨🚨"
+            msg = f"🚨 Контейнер {CONTAINER_NAME} остановлен! 🚨"
             send_alert(msg)
             was_running = False
         elif is_container_running() and not was_running:
-            msg = f"✅✅✅ Контейнер {CONTAINER_NAME} Запущен! ✅✅✅"
+            msg = f"✅ Контейнер {CONTAINER_NAME} Запущен! ✅"
             send_alert(msg)
             was_running = True
         time.sleep(30)  # Проверять каждые 30 секунд
